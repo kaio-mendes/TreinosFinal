9f137fad82416b845df6333e8abfe4c01a51e824
Kaio-Mendes <kaio.victormendes43@gmail.com>
2024-02-15 16:05:42 -0300

    tela metas, modal e calculo imc

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,13 +1,33 @@
-import styles from './Home.module.css'
+import styles from './Home.module.css';
+//import  {useState}  from  'react' ; 
+//instalar biblioteca date-fns e dependecias
+import { format } from 'date-fns';
+import { useState, useEffect } from 'react';
 
-function Home(){
-    return(
-        <div className={styles.head}>
-        
-            <h1>Você está na página principal</h1>
-        
+function Home() {   
+    const [currentDate, setCurrentDate] = useState(new Date());
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+          setCurrentDate(new Date());
+        }, 1000); // Atualiza a data a cada segundo
+    
+        return () => clearInterval(interval); // Limpa o intervalo quando o componente é desmontado
+      }, []);
+
+
+  return (
+    <div className={styles.container}>
+    <h1>Você está na página principal</h1>
+
+    <div className={styles.date}>
+            <h1>{format(currentDate, 'HH:mm')}</h1>
+        <div className={styles.day}>
+            <p>{format(currentDate, 'dd/MM/yyyy')}</p>
         </div>
-    )
+    </div>
+</div>
+);
 }
 
 export default Home;
--- src/pages/Metas/Metas.jsx
+++ src/pages/Metas/Metas.jsx
@@ -1,11 +1,22 @@
+import { useState } from 'react';
 import styles from './Metas.module.css'
+import Modal from './Modal';
+
 
 function Metas(){
+    const [openModal, setOpenModal] = useState(false);
+    const [ic, setIMC] = useState(null);
+
+
     return(
         <div className={styles.head}>
         
             <h1>Você está na página Metas</h1>
-        
+            <div className={styles.end}>
+            <button className={styles.abrir} onClick={()=> setOpenModal(true)}>Calcular IMC</button>
+            <Modal isOpen={openModal} setModalOpen={() => setOpenModal(!openModal)}>
+            </Modal>
+            </div>
         </div>
     )
 }
